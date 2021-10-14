fcfcb40e9595a43c7dc4f9cddb5ec8bb486793bc
lightning <lightning@lightningnoMacBook-Air.local>
2021-10-14 12:16:19 +0900

    過去コードを切り出し

--- src/main.py
+++ src/main.py
@@ -4,11 +4,18 @@ import re
 
 files = glob.glob('../pic/input/*.JPG')
 
-for pic in files:
-    print(pic)
-    image = Image.open(pic)
-    width, height = image.size
-    file_name = re.findall('input/(.*).JPG', pic)[0]
 
-    resized_image = image.resize((width//2, height//2), Image.LANCZOS)
-    resized_image.save(f"../pic/output/{file_name}_resized.JPG")
+def compress_multiple_images():
+    '''compress images that is in ../pic/input/
+    '''
+
+    for pic in files:
+        print(pic)
+        image = Image.open(pic)
+        width, height = image.size
+        file_name = re.findall('input/(.*).JPG', pic)[0]
+
+        resized_image = image.resize((width//2, height//2), Image.LANCZOS)
+        resized_image.save(f"../pic/output/{file_name}_resized.JPG")
+
+
